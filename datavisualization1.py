d49c68d46049739ecfca0594242d126fdc2a4fee
unknown <gebmania@gmail.com>
2017-11-15 11:44:42 +0900

    add result.png & draw title and label

--- datavisualization1.py
+++ datavisualization1.py
@@ -1,70 +1,54 @@
+
+#Data Visualization resport 1
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+# '****' and '***' mean missing data.
 df = pd.read_csv('ExcelFormattedGISTEMPDataCSV.csv', na_values=['****','***'])
 
+#get only monthly data 
 df = df.ix[:,:13]
 
+#I added new field to get the decade data easily.
 df['groupid'] = df.ix[:,0]//10
 
+#make groups with groupid
 groups = df.groupby('groupid')
 
+#get the means for each decade.
 result = groups.agg('mean')
 
+#well.. just restore index. it's not needed though.
 result = result.reset_index()
 
+#let's draw.
 plt.figure()
 
 
-
-years=[]
-
+#result.shape[0] is the number of records.
 for i in range(result.shape[0]):
-        years.append(188+i)
-
-for i in range(result.shape[0]):
+        #eliminate groupid and index
         out =  result.ix[i,2:]
         out2 = list(out)
+
+        #last data is for 2015 not 2019.
         if i == (result.shape[0]-1):
                 plt.plot(out2, label=str(188+i) + "0 ~ " + str(188+i) +'5')
         else:
                 plt.plot(out2, label=str(188+i) + "0 ~ " + str(188+i) +'9')
 
 
-plt.legend(loc=1)
+plt.legend(loc=1)           #on the right side.
+
+#some useful text.
+plt.xlabel("from 1880 to 2015 per decade")
+plt.ylabel("Deviation")
+plt.title("Global and Hemispheric Monthly Means")
+
+#draw it!
 plt.show()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#df.replace(22122,'English', inplace=True)
-#
-#stack = df.stack()
-#stack[ stack == '****'] = 0
-#stack[ stack == '***'] = 0
-#stack.unstack()
-plt.xlabel("years")
-plt.ylabel("temperature")
-plt.title("temperature")
-plt.axis([1880,2015,-100,100])
-plt.grid(True)
 
