92a7f08452531c85b63eca8b05469b941f503015
unknown <gebmania@gmail.com>
2017-11-15 09:37:27 +0900

    fix

--- datavisualization1.py
+++ datavisualization1.py
@@ -16,10 +16,21 @@ result = result.reset_index()
 
 plt.figure()
 
+
+
+years=[]
+
+for i in range(result.shape[0]):
+        years.append(188+i)
+
 for i in range(result.shape[0]):
         out =  result.ix[i,2:]
         out2 = list(out)
-        plt.plot(out2, label=str(i))
+        if i == (result.shape[0]-1):
+                plt.plot(out2, label=str(188+i) + "0 ~ " + str(188+i) +'5')
+        else:
+                plt.plot(out2, label=str(188+i) + "0 ~ " + str(188+i) +'9')
+
 
 plt.legend(loc=1)
 plt.show()
@@ -28,32 +39,32 @@ plt.show()
 
 
 
-data = {
-'1 ':[1,2,3],
-'2 ':[1,2,3],
-'3 ':[1,2,3],
-'4 ':[1,2,3],
-'5 ':[1,2,3],
-'6 ':[1,2,3],
-'7 ':[1,2,3],
-'8 ':[1,2,3],
-'9 ':[1,2,3],
-'10':[1,2,3],
-'11':[1,2,3],
-'12':[1,2,3],
-'13':[1,2,3]}
-
-
-x = pd.DataFrame(data)
-x.groupby('1')
 
 
 
-df2 = pd.read_csv('ExcelFormattedGISTEMPData2CSV.csv')
 
-//df.replace(22122,'English', inplace=True)
 
-stack = df.stack()
-stack[ stack == '****'] = 0
-stack[ stack == '***'] = 0
-stack.unstack()
+
+
+
+
+
+
+
+
+
+
+
+
+#df.replace(22122,'English', inplace=True)
+#
+#stack = df.stack()
+#stack[ stack == '****'] = 0
+#stack[ stack == '***'] = 0
+#stack.unstack()
+plt.xlabel("years")
+plt.ylabel("temperature")
+plt.title("temperature")
+plt.axis([1880,2015,-100,100])
+plt.grid(True)
+
